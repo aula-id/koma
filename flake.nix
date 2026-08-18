b874e8e24011fa05c46f5f7cd601ac571d179d87
zonblade <agungzonblade@gmail.com>
2026-08-18 08:54:01 +0000

    bump version to 0.3.20

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
 
-      version = "0.3.19";
+      version = "0.3.20";
 
       mkPackages = system:
         let
