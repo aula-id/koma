671adf758520f00d7d123059a34058232d822c60
zonblade <agungzonblade@gmail.com>
2026-09-05 20:23:34 +0000

    chore: bump version 0.4.3 → 0.4.4

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
 
-      version = "0.4.3";
+      version = "0.4.4";
 
       mkPackages = system:
         let
